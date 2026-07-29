6e3e5d40249039f1982541dff5bea60241864737
chaemin58 <sulv0309@naver.com>
2026-07-29 16:51:02 +0900

    fix: 코드 리뷰 반영

--- src/app/dashboard/[id]/_components/DashboardSkeletonUI.tsx
+++ src/app/dashboard/[id]/_components/DashboardSkeletonUI.tsx
@@ -1,5 +1,5 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css"; // 이거 추가
+import "react-loading-skeleton/dist/skeleton.css";
 
 export function DashboardSkeletonUI() {
   return (
